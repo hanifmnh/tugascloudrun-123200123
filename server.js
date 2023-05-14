269661e35c5d9c9b502254755409f97d0f713b19
Nur Hanif <72727969+hanifmnh@users.noreply.github.com>
2023-05-14 11:36:29 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require('http')
+const PORT = process.env.PORT || 9000
+http
+  .createServer((req, res) => {
+    console.log('New connection')
+    res.end('Hallo mas/mba, ini branch main')
+  })
+  .listen(PORT, () => console.log('Listening on', PORT))
